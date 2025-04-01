614a576f41f74b8ab940d31b4f5061590dff569c
Kaio Guerreiro <guerreirokaio@gmail.com>
2025-04-01 03:43:15 -0400

    git init

---
+++ src/componentes/produtos/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Carrinho from "../carrinho/index.jsx";
+import "./produtos.css";
+import { produtos } from "./produtos.js";
+import { BsCart2 } from "react-icons/bs";
+
+export default function Produtos() {
+    const [cartList, setCartList] = React.useState([]);
+    const [mostraCarrinho, setMostraCarrinho] = React.useState(false);
+
+    function addCarrinho(produto) {
+        setCartList((prev) => [...prev, produto]);
+    }
+
+    return (
+        <div className="conteinerProdutos">
+            <div className="botaoCarrinho">
+                <button onClick={() => setMostraCarrinho(true)}>
+                    <BsCart2 size={25} />
+                    <span className="quantidadeCarrinho">{cartList.length}</span>
+                </button>
+            </div>
+            
+        </div>
+
+    )
+}
